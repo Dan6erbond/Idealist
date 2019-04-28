b5ff68141bf19fba48f4c7293730ab2ee1087fc4
Dan6erbond <moravrav@gmail.com>
2019-04-28 18:33:35 +0200

    $idea command

--- idealist.py
+++ idealist.py
@@ -1,10 +1,15 @@
 import configparser
 import discord
+from datetime import datetime
 from discord.ext import commands
 
 desc = "Support Bot for Idealist by Dan6erbond"
 bot = commands.Bot("$", description=desc)
 
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(error)
 
 @bot.event
 async def on_member_join(member):
@@ -18,9 +23,61 @@ async def on_member_join(member):
 async def on_message(message):
     # check if we're in the introductions channel AND if the message has more than 25 words
     if message.channel.id == 568014240544325632 and len(message.content.split(" ")) > 25:
-        role = message.channel.guild.get_role(568016105147334677) # get the verified members role
+        keywords = ["skill", "set", "interest", "current", "project", "stuff", "i", "could", "use", "help", "with"]
+        for keyword in keywords:
+            if keyword not in message.content.lower():
+                return
+        await message.add_reaction("👍") # add the thumbsup reaction to show the user the introduction is good
+        role = message.channel.guild.get_role(568016105147334677)  # get the verified members role
         await message.author.add_roles(role) # assign it to the user
     await bot.process_commands(message)
+
+
+@bot.command(help="Submit an idea to #idea-lists through the bot.")
+@commands.has_role(568016105147334677)
+async def idea(ctx):
+    def check(m):
+        return m.author.id == ctx.author.id
+
+    await ctx.send("❓ What do you want to call your idea?")
+    name = await bot.wait_for("message", check=check)
+    name = name.content
+
+    await ctx.send("❓ Summarize your idea in between 20 and 200 words.")
+    elevator_pitch = await bot.wait_for("message", check=check)
+    elevator_pitch = elevator_pitch.content
+
+    await ctx.send("❓ What phase of the idea are you in?")
+    phase = await bot.wait_for("message", check=check)
+    phase = phase.content
+
+    await ctx.send("❓ What do you need help with?")
+    support = await bot.wait_for("message", check=check)
+    support = support.content
+
+    '''
+    await bot.get_channel(568044431933046787).send("New idea by {}!\n\n"
+                                                   "**Idea Name:** {}\n"
+                                                   "**Elevator Pitch:** {}\n"
+                                                   "**Phase:** {}\n"
+                                                   "**Would like to get support on:** {}".format(ctx.author.mention, name, elevator_pitch, phase, support))
+                                                   '''
+
+    embed = discord.Embed(
+        colour=discord.Colour(0).from_rgb(52, 152, 219)
+    )
+
+    embed.set_author(name="New idea by {}!".format(str(ctx.author)))
+    embed.set_footer(text="© Dan6erbond 2019", icon_url=bot.user.avatar_url)
+
+    embed.timestamp = datetime.utcnow()
+
+    embed.add_field(name="Idea Name", value=name, inline=False)
+    embed.add_field(name="Elevator Pitch", value=elevator_pitch, inline=False)
+    embed.add_field(name="Phase", value=phase, inline=False)
+    embed.add_field(name="Would Like To Get Support On", value=support, inline=False)
+
+    await bot.get_channel(568044431933046787).send(embed=embed)
 
 
 @bot.event
